0e67b5a3b2e9aac5e2af9eeb69e2c646b2ee084e
tph4nt0m <cosmodavid2009@gmail.com>
2023-06-08 14:25:32 -0500

    python

--- combatePokemon.py
+++ combatePokemon.py
@@ -1,4 +1,5 @@
 from random import randint
+import os
 
 VIDA_INICIAL_PIKACHU = 80
 VIDA_INICIAL_SQUIRTLE = 90
@@ -6,7 +7,17 @@ VIDA_INICIAL_SQUIRTLE = 90
 vidaPikachu = VIDA_INICIAL_PIKACHU
 vidaSquirtle = VIDA_INICIAL_SQUIRTLE
 
+       
+
 TAMAÑO_BARRA_VIDA = 20
+
+
+def borrar_pantalla():
+    if os.name == 'nt':  # Para sistemas Windows
+        os.system('cls')
+    else:  # Para sistemas Unix/Linux/Mac
+        os.system('clear')
+
 
 print("\n" + "En el nivel de hoy tendras que enfrentarte ante uno de los pokemones mas conocidos que es: Pikachu" + "\n"
       "Suerte!!!" + "\n")
@@ -16,18 +27,26 @@ while vidaPikachu > 0 and vidaSquirtle > 0:
     #Turno pikachu
     print("---------------------------------")
     print("Turno Pikachu")
-    ataquePikachu = randint(1,2)
+    ataquePikachu = randint(1, 3)
     
-    if ataquePikachu == "1":
+    if ataquePikachu == 1:
         #Bola Voltio
         print("Pikachu ataca con Bola Voltio")
         vidaSquirtle -= 10
     
-    else:
+    elif ataquePikachu == 2:
         #Onda Trueno
         print("Pikachu ataca con Onda Trueno")
         vidaSquirtle -= 11
 
+    else:
+        #No ataca
+        print("Pikachu No ataca")
+
+    if vidaPikachu < 0:
+        vidaPikachu = 0
+    if vidaSquirtle < 0:
+        vidaSquirtle = 0 
 
     barraVidaPikachu = int(vidaPikachu * TAMAÑO_BARRA_VIDA / VIDA_INICIAL_PIKACHU)
     print("Pikachu: [{}{}] ({}/{})".format("*" * barraVidaPikachu, " " * (TAMAÑO_BARRA_VIDA - barraVidaPikachu), vidaPikachu, VIDA_INICIAL_PIKACHU))
@@ -36,15 +55,16 @@ while vidaPikachu > 0 and vidaSquirtle > 0:
     print("Pikachu: [{}{}] ({}/{})".format("*" * barraVidaSquirtle, " " * (TAMAÑO_BARRA_VIDA - barraVidaSquirtle), vidaSquirtle, VIDA_INICIAL_SQUIRTLE))
 
     input("Enter para continuar... \n\n")
+    borrar_pantalla()
 
     #Turno Squirtle
     print("---------------------------------")
     print("Turno Squirtle")
     ataqueSquirtle= None
 
-    while ataqueSquirtle != "P" and ataqueSquirtle != "PA" and ataqueSquirtle != "B":
+    while ataqueSquirtle not in ["P", "PA", "B", "N"]:
 
-        ataqueSquirtle = input("¿Que ataque deseas realizar?  [P]--> Placaje, [PA]--> Pistola de Agua, [B]--> Burbuja: " )
+        ataqueSquirtle = input("¿Que ataque deseas realizar?  [P]--> Placaje, [PA]--> Pistola de Agua, [B]--> Burbuja, [N]--> No atacar: " )
         print("\n")
         
     if ataqueSquirtle == "P":
@@ -57,11 +77,19 @@ while vidaPikachu > 0 and vidaSquirtle > 0:
         print("Squirtle ataca con Pistola de de Agua")
         vidaPikachu -= 12
 
-    else:
+    elif ataqueSquirtle == "B":
         #Burbuja
         print("Squirtle ataca con Burbuja")
         vidaPikachu -= 9                    
 
+    else:
+        #No ataca
+        print("Squirtle No ataca")
+
+    if vidaPikachu < 0:
+        vidaPikachu = 0
+    if vidaSquirtle < 0:    
+        vidaSquirtle = 0 
 
     barraVidaPikachu = int(vidaPikachu * TAMAÑO_BARRA_VIDA / VIDA_INICIAL_PIKACHU)
     print("Pikachu: [{}{}] ({}/{})".format("*" * barraVidaPikachu, " " * (TAMAÑO_BARRA_VIDA - barraVidaPikachu), vidaPikachu, VIDA_INICIAL_PIKACHU))
@@ -70,7 +98,7 @@ while vidaPikachu > 0 and vidaSquirtle > 0:
     print("Pikachu: [{}{}] ({}/{})".format("*" * barraVidaSquirtle, " " * (TAMAÑO_BARRA_VIDA - barraVidaSquirtle), vidaSquirtle, VIDA_INICIAL_SQUIRTLE))        
     
     input("Enter para continuar... \n\n")
-
+    borrar_pantalla()
 
 #Se dice el ganador
 if vidaPikachu > vidaSquirtle:
