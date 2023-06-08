51a51e19c7c30edf0887f29fb04a2d56eb65efdd
tph4nt0m <cosmodavid2009@gmail.com>
2023-06-08 17:14:30 -0500

    python

--- maze.py
+++ maze.py
@@ -1,3 +1,5 @@
+import readchar
+import os
 
 POS_X = 0
 POS_Y = 1
@@ -7,19 +9,35 @@ MAP_HEIGHT = 15
 
 myPosition = [4, 3]
 
-#Dibujar mapa
-print(" ")
-print("+" + "-" * MAP_WIDTH * 3 + "+")
+while True:
+    #Draw map
+    print("+" + "-" * MAP_WIDTH * 3 + "+")
 
-for coordinate_y in range(MAP_HEIGHT):
-    print("|", end = "")
-    for coordinate_x in range(MAP_WIDTH):
-        if myPosition[POS_X] == coordinate_x and myPosition[POS_Y] == coordinate_y:
-            print(" @ ", end = "")
-        else:
-            print("   ", end = "")
-    print("|")
+    for coordinate_y in range(MAP_HEIGHT):
+        print("|", end = "")
+        for coordinate_x in range(MAP_WIDTH):
+            
+            #Draw initial position
+            if myPosition[POS_X] == coordinate_x and myPosition[POS_Y] == coordinate_y:
+                print(" @ ", end = "")
+            else:
+                print("   ", end = "")
+        print("|")
 
-print("+" + "-" * MAP_WIDTH * 3 + "+")   
+    print("+" + "-" * MAP_WIDTH * 3 + "+")   
 
+    #Move from position
+    direction = readchar.readchar()
 
+    if direction == "w":
+        myPosition[POS_Y] -= 1
+    elif direction == "s":
+        myPosition[POS_Y] += 1
+    elif direction == "a":
+        myPosition[POS_X] -= 1 
+    elif direction == "d":
+        myPosition[POS_X] += 1
+    elif direction == "q":
+        break
+    
+    os.system("cls")
