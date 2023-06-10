84c0ac46b437e528c297d8cd5fbf94c6bf22b8b8
tph4nt0m <cosmodavid2009@gmail.com>
2023-06-10 18:11:44 -0500

    python

--- maze.py
+++ maze.py
@@ -5,11 +5,33 @@ import random
 POS_X = 0
 POS_Y = 1
 
-MAP_WIDTH = 20
-MAP_HEIGHT = 15
 NUM_OF_MAP_OBJECTS = 25
 
-myPosition = [6, 3]
+#Create obstacle map
+obstacle_definition = """\
+##########################
+                       ####
+####    ############   ####
+######  ############   ####
+#####  ####
+######      ######  ######
+##########  ####         ##
+#######            ########
+#########   #####
+####      ############   ###
+######    ########         #
+########                 ###
+######## #########
+#####    ###########    ####
+############################\
+"""
+
+#Make sure all lines are the same length
+max_line_length = max(len(line) for line in obstacle_definition.split("\n"))
+obstacle_definition = [line.ljust(max_line_length) for line in obstacle_definition.split("\n")]
+
+
+myPosition = [0, 1]
 mapObjects= []
 tail = []
 tail_length = 0
@@ -21,6 +43,14 @@ died = False
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
+#Divide into list of lists
+#List comprehension
+obstacle_definition = [list(row) for row in obstacle_definition]
+#[position y][position x]
+
+MAP_WIDTH = len(obstacle_definition[0])
+MAP_HEIGHT = len(obstacle_definition)
+
 #Main
 while not end_game:
     
@@ -28,20 +58,21 @@ while not end_game:
     
     #Generate random objects
     while len(mapObjects) < NUM_OF_MAP_OBJECTS:
-        new_position = [random.randint(0, MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
+        new_position = [random.randint(0, MAP_WIDTH - 1), random.randint(0, MAP_HEIGHT - 1)]
     
-        if new_position not in mapObjects and new_position != myPosition:
+        if new_position not in mapObjects and new_position != myPosition and \
+                obstacle_definition[new_position[POS_Y]][new_position[POS_X]] != "#":
             mapObjects.append(new_position)
     
     #Draw map
-    print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print("+" + "-" * MAP_WIDTH * 2 + "+")
 
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end = "")
 
         for coordinate_x in range(MAP_WIDTH):
             
-            char_to_draw = " "
+            char_to_draw = "  "
             object_in_cell = None
             tail_in_cell = None
 
@@ -49,19 +80,19 @@ while not end_game:
             #Draw objects
             for map_object in mapObjects:
                 if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
-                    char_to_draw = "*"
+                    char_to_draw = " *"
                     object_in_cell = map_object
                     
 
             #Draw tail
             for tail_piece in tail:
                 if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == coordinate_y:
-                    char_to_draw = "@"
+                    char_to_draw = " @"
                     tail_in_cell = tail_piece
 
             #Draw initial position
             if myPosition[POS_X] == coordinate_x and myPosition[POS_Y] == coordinate_y:
-                char_to_draw = "@"
+                char_to_draw = " @"
                 
                 #Remove objecto
                 if object_in_cell:
@@ -75,46 +106,49 @@ while not end_game:
                     end_game = True
                     died = True
 
+            if obstacle_definition[coordinate_y][coordinate_x] == "#":
+                char_to_draw = "##"
+            
 
-            print(" {} ".format(char_to_draw), end = "")
+            print("{}".format(char_to_draw), end = "")
         print("|")
 
-    print("+" + "-" * MAP_WIDTH * 3 + "+")  
+    print("+" + "-" * MAP_WIDTH * 2 + "+")  
 
     #Move from position
     direction = readchar.readchar()
+    new_position = None
 
     if direction == "w":
-        #Add last position in tail
-        tail.insert(0, myPosition.copy())
-        #Cut tail
-        tail = tail[:tail_length] 
-        #Move
-        myPosition[POS_Y] -= 1
-        #Pass limit map --> move star map
-        myPosition[POS_Y] %= MAP_HEIGHT
+        #Search if new position there is a obstacle
+        new_position = [myPosition[POS_X], (myPosition[POS_Y] - 1) % MAP_WIDTH]
+        
     
     elif direction == "s":
-        tail.insert(0, myPosition.copy())
-        tail = tail[:tail_length] 
-        myPosition[POS_Y] += 1
-        myPosition[POS_Y] %= MAP_HEIGHT
+        new_position = [myPosition[POS_X], (myPosition[POS_Y] + 1) % MAP_WIDTH]
+        
     
     elif direction == "a":
-        tail.insert(0, myPosition.copy())
-        tail = tail[:tail_length] 
-        myPosition[POS_X] -= 1
-        myPosition[POS_X] %= MAP_WIDTH 
+        new_position = [(myPosition[POS_X] - 1) % MAP_WIDTH, myPosition[POS_Y]]
+        
     
     elif direction == "d":
-        tail.insert(0, myPosition.copy())
-        tail = tail[:tail_length] 
-        myPosition[POS_X] += 1
-        myPosition[POS_X] %= MAP_WIDTH 
+        new_position = [(myPosition[POS_X] + 1) % MAP_WIDTH, myPosition[POS_Y]]
+        
     
     elif direction == "q":
         end_game = True
     
+    if new_position:
+        if obstacle_definition[new_position[POS_Y]][new_position[POS_X]] != "#":
+            #Add last position in tail
+            tail.insert(0, myPosition.copy())
+            #Cut tail
+            tail = tail[:tail_length] 
+            #Move
+            myPosition = new_position
+
+
     clear_screen()
 
 if died:
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-h
+obstacle_definition = [list(row) for row in obstacle_definition.split("\n")]
